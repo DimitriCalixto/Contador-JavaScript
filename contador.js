cefd86c82d923ef2e361baf556a2d4c7a1b6c878
Dimitri Calixto Arruda Martins <57041642+DimitriCalixto@users.noreply.github.com>
2025-08-27 08:20:13 -0300

    [Create] Contador

---
+++ contador.js
@@ -0,0 +1,13 @@
+let contador1 = 10;
+
+while (contador1 >= 0){
+    alert("Contador: " + contador1);
+    contador1--;
+}
+
+let contador2 = 0;
+
+while (contador2 <= 10){
+    alert("Contador: " + contador2);
+    contador2++;
+}
